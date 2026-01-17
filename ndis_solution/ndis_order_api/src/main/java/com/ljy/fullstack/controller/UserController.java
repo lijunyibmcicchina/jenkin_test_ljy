f00feaa9c1c27e5c8cb79bf1d18c37de22d9de13
JUN YING LI <lijunyibmcicchina@gmail.com>
2026-01-17 20:07:06 +0800

    update on 1/17

--- ndis_solution/ndis_order_api/src/main/java/com/ljy/fullstack/controller/UserController.java
+++ ndis_solution/ndis_order_api/src/main/java/com/ljy/fullstack/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("api/users")
 public class UserController {
     private final NDISServiceService ndisServiceService;
 
